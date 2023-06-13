cba0e943bf6d11affd289bcdf32f9f4394be0028
오현재 <55138387+hangooksaram@users.noreply.github.com>
2023-06-13 23:36:04 +0900

    FEAT:Styled 컴포넌트 추가
    - Input, Button 컴포넌트 추가

---
+++ src/components/styles/Button.tsx
@@ -0,0 +1,8 @@
+import styled from "@emotion/styled";
+
+const Button = styled.button((props) => ({
+  width: "100%",
+  height: "24px",
+}));
+
+export default Button;
---
+++ src/components/styles/Input.tsx
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+
+const Input = styled.input`
+  width: 100%;
+  height: 24px;
+  text-decoration: none;
+`;
+
+export default Input;
